9024b08977927a0590870423d0b3c363e7015ea3
Mario <villamario7@gmail.com>
2025-04-06 12:49:01 +0200

    archivo de rutas

--- museo-angular/src/app/app.routes.ts
+++ museo-angular/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { ObraDetalleComponent } from './pages/obra-detalle/obra-detalle.component';
+import { PagoComponent } from './pages/pago/pago.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'catalogo', component: CatalogoComponent },
+  { path: 'obra/:id', component: ObraDetalleComponent },
+  { path: 'pago', component: PagoComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: '**', redirectTo: '' }
+];
