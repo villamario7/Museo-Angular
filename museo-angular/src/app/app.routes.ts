0167bee8b1cd572a99c95b9639e96057b3d437cf
Cq - mainDesktop <jorge.dediegojimenez@cieep.com>
2025-04-06 17:15:13 +0200

    router navbar

--- museo-angular/src/app/app.routes.ts
+++ museo-angular/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component : HomeComponent},
+    {path: 'navbar', component : NavbarComponent},
+    {path: 'footer', component : FooterComponent},
+    {path: 'catalogo', component : CatalogoComponent},
+    {path: 'about-us', component : AboutUsComponent},
+    {path: 'home', component : HomeComponent},
+];
--- museo-angular/src/app/components/navbar/navbar.component.ts
+++ museo-angular/src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
