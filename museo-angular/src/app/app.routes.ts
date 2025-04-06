011f2fcbab8d7ede047916f4c2a15179eff8d446
Cq - mainDesktop <jorge.dediegojimenez@cieep.com>
2025-04-06 17:34:37 +0200

    mas rutas navbar

--- museo-angular/src/app/app.routes.ts
+++ museo-angular/src/app/app.routes.ts
@@ -4,6 +4,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CatalogoComponent } from './pages/catalogo/catalogo.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { PagoComponent } from './pages/pago/pago.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
@@ -11,6 +15,9 @@ export const routes: Routes = [
     {path: 'navbar', component : NavbarComponent},
     {path: 'footer', component : FooterComponent},
     {path: 'catalogo', component : CatalogoComponent},
+    {path: 'contacto', component : ContactoComponent},
     {path: 'about-us', component : AboutUsComponent},
-    {path: 'home', component : HomeComponent},
+    {path: 'login', component : LoginComponent},
+    {path: 'register', component : RegisterComponent},
+    {path: 'pago', component : PagoComponent},
 ];
