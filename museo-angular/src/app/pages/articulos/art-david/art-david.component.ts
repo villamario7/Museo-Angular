fd04cca35e0a07ed2b88bde8d0fd88f5f4d27db2
Cq - mainDesktop <jorge.dediegojimenez@cieep.com>
2025-04-20 23:42:52 +0200

    volver button

--- museo-angular/src/app/pages/articulos/art-david/art-david.component.ts
+++ museo-angular/src/app/pages/articulos/art-david/art-david.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
 @Component({
   selector: 'app-art-david',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive ],
   templateUrl: './art-david.component.html',
   styleUrl: './art-david.component.css'
 })
--- museo-angular/src/app/pages/articulos/art-madonna/art-madonna.component.ts
+++ museo-angular/src/app/pages/articulos/art-madonna/art-madonna.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
 @Component({
   selector: 'app-art-madonna',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './art-madonna.component.html',
   styleUrl: './art-madonna.component.css'
 })
