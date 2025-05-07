23dfbfaac81ea0407326209df0295cab17510b2a
Cq - mainDesktop <jorge.dediegojimenez@cieep.com>
2025-05-07 14:50:07 +0200

    cleaner exhibicion

--- museo-angular/src/app/common/dataExposicion.ts
+++ museo-angular/src/app/common/dataExposicion.ts
@@ -1,13 +1,11 @@
 export interface dataExposicion{
-    evento: string
-    fecha: string
     listaExpos : exposicion[]
 }
 
 export class exposicion{
-    titulo: string
-    descripcion: string
-    imagen : string
+    titulo: string;
+    descripcion: string;
+    imagen : string;
     constructor(Titulo: string, Descripcion: string, Imagen : string){
         this.titulo = Titulo;
         this.descripcion = Descripcion;
--- museo-angular/src/app/pages/home/home.component.ts
+++ museo-angular/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component,  OnInit } from '@angular/core';
 import { DataService } from '../../servicios/data.service';
 import { dataExposicion } from '../../common/dataExposicion';
+
 @Component({
   selector: 'app-home',
   imports: [],
@@ -9,7 +10,7 @@ import { dataExposicion } from '../../common/dataExposicion';
 })
 export class HomeComponent implements OnInit{
   constructor( private dataservice: DataService){}
-  
+
     ngOnInit(): void {
       this.loadExposiciones();
     }
@@ -18,7 +19,7 @@ export class HomeComponent implements OnInit{
       this.dataservice.getExposiciones().subscribe(
         {
           next: (data)=>{
-            this.dataExposicion = data;
+            this.dataExposiciones = data;
             console.log(data);
           },
           error: (error)=>{
@@ -31,5 +32,17 @@ export class HomeComponent implements OnInit{
       )
   
     }
-    dataExposicion! : dataExposicion;
+    dataExposiciones! : dataExposicion;
+
+    expoIndex= 0; //Como lo que recibe es una lista del json, desde aquí controlamos el elemento elegido en lugar de desde el html
+    getIndex(){
+      /*
+      setInterval(() =>{
+        if(this.expoIndex==0) this.expoIndex=1;
+        if(this.expoIndex==1) this.expoIndex=0;
+        console.log(this.expoIndex);
+      }, 5000);
+      */
+      return this.dataExposiciones.listaExpos[this.expoIndex];
+    }
 }
